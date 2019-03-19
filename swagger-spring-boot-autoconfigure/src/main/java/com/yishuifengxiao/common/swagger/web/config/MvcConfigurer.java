970a5c6c3c2d8d96fd9e9183cbb203ebeb30fc49
yishui <yishui@DESKTOP-DQH38FD>
2019-03-19 10:43:31 +0800

    修复bug

--- swagger-spring-boot-autoconfigure/src/main/java/com/yishuifengxiao/common/swagger/web/config/MvcConfigurer.java
+++ swagger-spring-boot-autoconfigure/src/main/java/com/yishuifengxiao/common/swagger/web/config/MvcConfigurer.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Version 0.0.1
  */
 @EnableAutoConfiguration
-public class MvcConfigurerAutoConfiguration implements WebMvcConfigurer {
+public class MvcConfigurer implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("doc.html").addResourceLocations("classpath*:/META-INF/resources/");
