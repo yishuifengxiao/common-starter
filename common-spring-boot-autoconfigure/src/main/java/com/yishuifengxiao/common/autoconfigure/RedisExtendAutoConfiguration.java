ede8a5e5801fb39b290d4a88ae9a2b58c2c6439c
石易 <zhiyubujian@163.com>
2019-07-17 23:33:20 +0800

    优化代码

--- common-spring-boot-autoconfigure/src/main/java/com/yishuifengxiao/common/autoconfigure/RedisExtendAutoConfiguration.java
+++ common-spring-boot-autoconfigure/src/main/java/com/yishuifengxiao/common/autoconfigure/RedisExtendAutoConfiguration.java
@@ -1,8 +1,14 @@
 package com.yishuifengxiao.common.autoconfigure;
 
+import java.net.UnknownHostException;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -11,8 +17,19 @@ import com.yishuifengxiao.common.validation.repository.CodeRepository;
 import com.yishuifengxiao.common.validation.repository.impl.RedisCodeRepository;
 
 @Configuration
-@ConditionalOnBean(name = "redisTemplate")
+@ConditionalOnClass(RedisOperations.class)
 public class RedisExtendAutoConfiguration {
+	
+
+	@Bean
+	@ConditionalOnMissingBean(name = "redisTemplate")
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+			throws UnknownHostException {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
+	
 	/**
 	 * 记住密码策略【存储在redis数据库中】
 	 * 
