2e61d8023cb45c31142b073a1c8b1e8c4668cde1
yishui <zhiyubujian@163.com>
2019-10-15 10:37:55 +0800

    修改bug

--- common-spring-boot-autoconfigure/src/main/java/com/yishuifengxiao/common/validation/code/image/ImageCodeGenerator.java
+++ common-spring-boot-autoconfigure/src/main/java/com/yishuifengxiao/common/validation/code/image/ImageCodeGenerator.java
@@ -10,6 +10,7 @@ import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -24,14 +25,31 @@ import com.yishuifengxiao.common.validation.generator.CodeGenerator;
  * @version 0.0.1
  */
 public class ImageCodeGenerator implements CodeGenerator {
+	/**
+	 * 默认验证码的长度
+	 */
+	private final static int DEFAULT_LENGTH = 4;
+	/**
+	 * x方向上默认的内边距
+	 */
+	private final static int DEFAULT_X_PADDING = 6;
+
+	/**
+	 * y方向上默认的内边距
+	 */
+	private final static int DEFAULT_Y_PADDING = 8;
 	private CodeProperties codeProperties;
 
 	@Override
 	public ImageCode generate(ServletWebRequest servletWebRequest) {
+		// 图形验证码的宽度
 		int width = ServletRequestUtils.getIntParameter(servletWebRequest.getRequest(), "width",
 				codeProperties.getImage().getWidth());
+		// 图形验证码的高度
 		int height = ServletRequestUtils.getIntParameter(servletWebRequest.getRequest(), "height",
 				codeProperties.getImage().getHeight());
+		// 图形验证码的长度
+		int length = codeProperties.getImage().getLength() > 0 ? codeProperties.getImage().getLength() : DEFAULT_LENGTH;
 		// 生成一个图片对象
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
@@ -57,19 +75,51 @@ public class ImageCodeGenerator implements CodeGenerator {
 
 		// 生成四位的随机数
 		String sRand = "";
-		for (int i = 0; i < codeProperties.getImage().getLength(); i++) {
-			String rand = RandomStringUtils.random(1, codeProperties.getImage().isContainLetter(),
-					codeProperties.getImage().isContainNumber());
-			// 防止生成为null
-			rand = StringUtils.isNotBlank(rand) ? rand : new Random().nextInt(10) + "";
+
+		for (int i = 0; i < length; i++) {
+
+			int xCoordinate = ((width - DEFAULT_X_PADDING) / length) * i + DEFAULT_X_PADDING;
+			int yCoordinate = height - DEFAULT_Y_PADDING;
+			//绘制文字
+			String rand = generate(xCoordinate, yCoordinate, codeProperties.getImage().isContainLetter(),
+					codeProperties.getImage().isContainNumber(), g);
 			sRand += rand;
-			// 字符的颜色随机
-			g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
-			g.drawString(rand, 13 * i + 6, 22);
+
 		}
 
 		g.dispose();
 		return new ImageCode(codeProperties.getImage().getExpireIn(), sRand, image);
+	}
+
+	/**
+	 * 根据要求生成字符并将字符绘制到底片上
+	 * 
+	 * @param xCoordinate     绘制时的x坐标
+	 * @param yCoordinate     绘制时的y坐标
+	 * @param isContainLetter 是否包含字母
+	 * @param isContainNumber 是否包含数字
+	 * @param g
+	 * @return 绘制到底片上的文字
+	 */
+	private String generate(int xCoordinate, int yCoordinate, boolean isContainLetter, boolean isContainNumber,
+			Graphics g) {
+		Random random = new Random();
+		String rand = "";
+		if (isContainLetter && !isContainNumber) {
+			// 只包含字母
+			rand = RandomStringUtils.random(1, false, false);
+		} else if (isContainNumber && !isContainLetter) {
+			// 只包含数字
+			rand = RandomUtils.nextInt(0, 10) + "";
+		} else {
+			rand = RandomStringUtils.random(1, false, true);
+		}
+		// 判断是否出现空白字符
+		rand = StringUtils.isNotBlank(rand) ? rand : random.nextInt(10) + "";
+		// 字符的颜色随机
+		g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
+		g.drawString(rand, xCoordinate, yCoordinate);
+		return rand;
 	}
 
 	/**
